bbc0fda5d8e380fb848de3b711a5e8a5310ca1bc
Sunil Unka <sunil.unka@gmail.com>
2016-03-02 00:06:58 +1300

    Updated version of firebase to fix issue with AngularJS HTML5mode and redirects.

--- browser/js/common/authModule/auth-module.js
+++ browser/js/common/authModule/auth-module.js
@@ -52,7 +52,8 @@
        */
 
       loginWithExternalProvider: provider => {
-        return authRef.$authWithOAuthPopup(provider, function(error, authData){
+        return authRef.$authWithOAuthRedirect(provider, function(error, authData){
+          console.log("REDIRECTING...")
           if(error) {
             console.warn("Sorry an error occured: ", error);
             return error;
@@ -95,7 +96,7 @@
 
     this.reportAuthState = () => {
       authRef.$onAuth(authData => {
-
+        console.log("AUTH DATA FROM REDIRECT: ", authData)
         /* CASE: If authData is returned, but user data is available from SessionService
           => Means that the user has logged in with an external media service, but has not registered data on the site.
          */
@@ -118,7 +119,8 @@
           $rootScope.$broadcast('loggedOut');
           console.warn("Logged out!");
           // console.log("SESSION USER LOGGED OUT: ", SessionService.user);
-
+        } else if(!SessionService.user && !authData){
+          console.log("AUTH DATA: ", authRef.$getAuth());
         } else if (SessionService.user) {
           $rootScope.$broadcast('loggedIn', SessionService.user)
           return /* No need to return anything, user is still signed in */
