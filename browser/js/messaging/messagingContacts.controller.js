16dcf3af83fc9cdcac9272b43510d37f73f8ff86
Sunil Unka <sunil.unka@gmail.com>
2016-06-16 21:34:06 +0100

    Cloned functionality from management users to look users up for chat. Now working on chart of users chats.

--- browser/js/messaging/messagingContacts.controller.js
+++ browser/js/messaging/messagingContacts.controller.js
@@ -1,8 +1,42 @@
-app.controller("MessagingContactsCtrl", function($scope, Users, Platforms, Categories, Events, loggedInUser){
+app.controller("MessagingContactsCtrl", function($scope, Platforms, Categories, Events, loggedInUser, $timeout){
 
   $scope.platforms = Platforms;
   $scope.categories = Categories;
   $scope.events = Events;
-  $scope.users = Users;
   $scope.current = loggedInUser;
+
+  $scope.searchResults = [];
+
+  $scope.categories = Categories;
+
+  $scope.platforms = Platforms;
+
+  $scope.events = Events;
+
+  $scope.showMessage = false;
+
+  $scope.activity;
+
+  $scope.userFilterParams = {};
+
+  $scope.filterUsers = function(){
+    for(var param in $scope.filterParams){
+      $scope.userFilterParams[param] = $scope.filterParams[param];
+    }
+  }
+
+
+  $scope.$on("resultsReceived", function(event, value){
+    if(!value.length){
+      $scope.activity = "No users found";
+      $scope.showMessage = true;
+    } else {
+      $scope.showMessage = false;
+    }
+    angular.copy(value, $scope.searchResults);
+    $timeout(function(){
+      $scope.$apply();
+    }, 1)
+  })
+
 })
