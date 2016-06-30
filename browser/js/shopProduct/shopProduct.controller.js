f57fecf63cd9902d71323ce92b2831f04a33d34a
Sunil Unka <sunil.unka@gmail.com>
2016-06-30 22:45:41 +0100

    Completed responsive formatting of product page.

--- browser/js/shopProduct/shopProduct.controller.js
+++ browser/js/shopProduct/shopProduct.controller.js
@@ -17,28 +17,33 @@ app.controller("ShopProductCtrl", function($scope, ShopifyFactory, shopProduct, 
     })[0];
   }
 
-  $scope.updateSelectedVariant = function(option, newValue){
-    // option.selected = newValue;
-    option.value = newValue;
-    option.selected = newValue;
+  var setSelectedVariantAvailable = function(product){
     if(!$scope.product.selectedVariant || !$scope.product.selectedVariant.attrs.variant.available){
       $scope.notAvailable = true;
     } else {
       $scope.notAvailable = false;
     }
+  }
+
+  $scope.updateSelectedVariant = function(option, newValue){
+    // option.selected = newValue;
+    option.value = newValue;
+    option.selected = newValue;
+    setSelectedVariantAvailable($scope.product);
     // angular.copy($scope.optionObject[option.name][newValue], $scope.availableVars[option.name]);
     // console.log("AVAILABLE VARIANTS: ", $scope.availableVars);
     return option;
   }
 
-  var setSelectedOptionsOnLoad = function(productOptions){
-    productOptions.forEach(function(option){
+  var setSelectedOptionsOnLoad = function(product){
+    product.options.forEach(function(option){
       option.value = option.selected;
       // $scope.availableVars[option.name] = [];
     })
+    setSelectedVariantAvailable(product)
   }
 
-  setSelectedOptionsOnLoad($scope.product.options);
+  setSelectedOptionsOnLoad($scope.product);
 
   $scope.addProductToCart = function(){
     ShopifyService.addToCart({
