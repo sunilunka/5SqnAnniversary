e42169b32796fc7e50b2e62cb460cda7e2961915
Sunil Unka <sunil.unka@gmail.com>
2016-07-02 23:34:42 +0100

    Working on further styling. Added price to sho-front-product.

--- browser/js/common/directives/shop-front-product/shop-front-product.js
+++ browser/js/common/directives/shop-front-product/shop-front-product.js
@@ -7,9 +7,12 @@ app.directive("shopFrontProduct", function(ShopifyService, ShopifyFactory){
     },
     link: function(scope, element, attrs){
       var product = scope.product;
+      var selectedVar = scope.product.selectedVariant;
       scope.title = product.title;
       scope.imageSrc = product.images[0].src;
+      scope.price = selectedVar.price;
       console.log("PRODUCT DETAILS: ", product);
+
 
     }
   }
