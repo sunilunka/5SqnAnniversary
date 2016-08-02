d910905f7438aedadfdbfb798688ea0264a0e826
Sunil Unka <sunil.unka@gmail.com>
2016-08-02 17:23:03 +0100

    Have order filtering by number working. Working on order GET route to query for different order params.

--- browser/js/management-shop-orders/management-shop-orders.controller.js
+++ browser/js/management-shop-orders/management-shop-orders.controller.js
@@ -2,4 +2,39 @@ app.controller("ManagementShopOrdersCtrl", function($scope, allOrders){
 
   $scope.orders = allOrders;
 
+  $scope.noOrders = true;
+
+  $scope.processingQuery = false;
+
+  $scope.currentView = "All";
+
+  $scope.viewOptions = [{
+    key: "all",
+    name: "All"
+  },
+  {
+    key: "paymentState",
+    name: "Paid"
+  },
+  {
+    key: "paymentState",
+    name: "Payment Pending"
+  }];
+
+  $scope.setOrderView = function(valueObj){
+    $scope.currentView = valueObj.name;
+  }
+
+  $scope.filterParams = {};
+
+  $scope.findOrderNumber = function(){
+    if($scope.orderRef){
+      $scope.filterParams['order_ref'] = "5SQN-75-" + $scope.orderRef;
+    }
+  }
+
+  $scope.getOrderQueryResults = function(valueObj){
+
+  }
+
 })
