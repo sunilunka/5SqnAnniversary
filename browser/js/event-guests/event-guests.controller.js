f07fca4f4003807285f684a279dfcb23434166e0
Sunil Unka <sunil.unka@gmail.com>
2016-08-21 21:32:30 +0100

    Added filtering data to event guest list.

--- browser/js/event-guests/event-guests.controller.js
+++ browser/js/event-guests/event-guests.controller.js
@@ -6,12 +6,26 @@ app.controller("EventGuestsCtrl", function($scope, allEvents, EventGuestFactory)
 
   $scope.guests = [];
 
+  $scope.searchParams = {};
+
   $scope.loadEventGuests = function(eventId){
     $scope.selectedEvent = eventId;
     EventGuestFactory.getAllEventAttendees(eventId)
     .then(function(resultArray){
+      /* Reset filter params if they have been set. */
+      angular.copy({}, $scope.searchParams);
       angular.copy(resultArray, $scope.guests);
     })
+  }
+
+  $scope.filterAttendees = function(){
+      $scope.searchParams["registeredAttendee"] = $scope.searchName;
+      console.log("SEARCH NAME: ", $scope.searchName);
+  }
+
+  $scope.resetSearch = function(event){
+    event.preventDefault();
+    angular.copy({}, $scope.searchParams);
   }
 
 })
