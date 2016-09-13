5ed11294d84bbd1462499651b519afd3b7b8d6ba
Sunil Unka <sunil.unka@gmail.com>
2016-09-13 08:14:17 +0100

    Updated referred attendee to clear authenticating overlay.

--- browser/js/referredNewAttendee/referredNewAttendee.controller.js
+++ browser/js/referredNewAttendee/referredNewAttendee.controller.js
@@ -1,9 +1,12 @@
-app.controller("referredNewAttendeeCtrl", function($scope, events, categories, platforms){
+app.controller("referredNewAttendeeCtrl", function($scope, events, categories, platforms, $rootScope){
   /* Controller used if user tries to login, but has no account. */
   $scope.events = events;
 
   $scope.categories = categories;
 
   $scope.platforms = platforms;
+
+  // Auth would have been in progress, make sure to remove auth overlay.
+  $rootScope.$broadcast("authComplete");
 
 })
